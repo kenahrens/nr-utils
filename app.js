b71ae1964e983c4489a12deccfbe466f957b68a7
kenahrens <ken.ahrens@gmail.com>
2016-03-25 15:36:27 -0400

    Adding basic koa p2

---
+++ app.js
@@ -0,0 +1,25 @@
+var koa = require('koa');
+var app = koa();
+
+// Set X-Response-Time
+app.use(function *(next) {
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  this.set('X-Response-Time', ms + 'ms');
+});
+
+// Logger
+app.use(function *(next)) {
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// Response
+app.use(function *() {
+  this.body = 'Hello World!';
+});
+
+app.listen(3000);
